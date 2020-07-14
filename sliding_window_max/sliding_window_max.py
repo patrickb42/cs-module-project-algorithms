e8e43c39da146cf6146720d3eb753e38dc5ffe96
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-07-14 01:43:05 -0400

    sliding_window_max.py

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,14 +3,16 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    def helper(index):
+        nonlocal nums
+        nonlocal k
+        return max(nums[index : index + k])
 
-    pass
+    return list(map(helper, range(len(nums) - (k - 1))))
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
-
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
